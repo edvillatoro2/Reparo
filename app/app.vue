0b032f40880092acf05edb055c3e9abe1cb17fe7
edvillatoro2 <edvillatoro2@gmail.com>
2026-07-03 15:35:04 -0400

    adds google font

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,21 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap");
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Inter", system-ui, sans-serif;
+  background-color: #e4d1b9;
+  color: #2c1a0e;
+}
+</style>
